a1445945a4da2ba1670dbb85d0bfdb8d28653d0f
Loïc GIRON DIT METAZ <lgironditmetaz@smartadserver.com>
2025-04-28 16:54:32 +0200

    Keymap refactoring for easier readability

--- keyboards/splitkb/aurora/corne/keymaps/custom/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/custom/keymap.c
@@ -20,10 +20,30 @@ combo_t key_combos[] = {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_split_3x6_3(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_LCTL, LSFT_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G, KC_H, RGUI_T(KC_J), LALT_T(KC_K), RCTL_T(KC_L), RSFT_T(KC_SCLN), KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ESC, KC_LGUI, MO(2), LSFT_T(KC_SPC), RSFT_T(KC_ENT), MO(1), KC_RALT),
-    [1] = LAYOUT_split_3x6_3(KC_TAB, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, KC_LCTL, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_DEL, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LGUI, MO(3), LSFT_T(KC_ESC), RSFT_T(KC_ENT), KC_TRNS, KC_RALT),
-    [2] = LAYOUT_split_3x6_3(KC_TAB, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_LCTL, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, KC_MINS, RGUI_T(KC_EQL), LALT_T(KC_LBRC), RCTL_T(KC_RBRC), RSFT_T(KC_BSLS), KC_GRV, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LGUI, KC_TRNS, LSFT_T(KC_SPC), RSFT_T(KC_BSPC), MO(3), KC_RALT),
-    [3] = LAYOUT_split_3x6_3(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_VOLD, KC_VOLU, KC_MEDIA_PLAY_PAUSE, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP, KC_NO, KC_NO, KC_NO, QK_BOOT, KC_LGUI, KC_TRNS, LSFT_T(KC_SPC), RSFT_T(KC_ENT), KC_TRNS, KC_RALT)
+    [0] = LAYOUT_split_3x6_3(
+        KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,           KC_Y,           KC_U,         KC_I,         KC_O,         KC_P,            KC_BSPC,
+        KC_LCTL, LSFT_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G,           KC_H,           RGUI_T(KC_J), LALT_T(KC_K), RCTL_T(KC_L), RSFT_T(KC_SCLN), KC_QUOT,
+        KC_LSFT, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,           KC_N,           KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_ESC,
+                                             KC_LGUI,      MO(2),        LSFT_T(KC_SPC), RSFT_T(KC_ENT), MO(1),        KC_RALT
+    ),
+    [1] = LAYOUT_split_3x6_3(
+        KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,        KC_CIRC,        KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+        KC_LCTL, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO,          KC_LEFT,        KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_DEL,
+        KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,          KC_NO,          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                   KC_LGUI, MO(3),   LSFT_T(KC_ESC), RSFT_T(KC_ENT), KC_TRNS, KC_RALT
+    ),
+    [2] = LAYOUT_split_3x6_3(
+        KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,            KC_7,           KC_8,            KC_9,            KC_0,            KC_BSPC,
+        KC_LCTL, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO,          KC_MINS,         RGUI_T(KC_EQL), LALT_T(KC_LBRC), RCTL_T(KC_RBRC), RSFT_T(KC_BSLS), KC_GRV,
+        KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,          KC_NO,           KC_NO,          KC_NO,           KC_NO,           KC_NO,           KC_NO,
+                                   KC_LGUI, KC_TRNS, LSFT_T(KC_SPC), RSFT_T(KC_BSPC), MO(3),          KC_RALT
+    ),
+    [3] = LAYOUT_split_3x6_3(
+        KC_F1, KC_F2, KC_F3, KC_F4,   KC_F5,   KC_F6,          KC_F7,              KC_F8,            KC_F9,               KC_F10, KC_F11, KC_F12,
+        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,          KC_VOLD,            KC_VOLU,          KC_MEDIA_PLAY_PAUSE, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,          KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP, KC_NO,               KC_NO, KC_NO, QK_BOOT,
+                             KC_LGUI, KC_TRNS, LSFT_T(KC_SPC), RSFT_T(KC_ENT),     KC_TRNS,          KC_RALT
+    )
 };
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
